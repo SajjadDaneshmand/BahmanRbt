1078f9eb1156096e1f50a04b77dfbc03034dba0d
sajjad daneshmand <Mr.SajjadDaneshmand@gmail.com>
2021-07-20 16:49:58 +0430

    page loaded fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import persian_character
 
 # standard
+import hashlib
 import time
 import csv
 
@@ -9,10 +10,11 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# BeautifulSoup
+# BeautifulSoup and Requests
 from bs4 import BeautifulSoup
 
 
@@ -22,7 +24,7 @@ def table_catcher(src):
 
 # handeling ajax
 def wait_for_ajax(driver, selector):
-    return WebDriverWait(driver, 60).until(EC.presence_of_element_located(selector))
+    return WebDriverWait(driver, delay).until(EC.presence_of_element_located(selector))
 
 # get all number of pages
 def number_of_page():
@@ -32,6 +34,12 @@ def number_of_page():
     except:
         return False
 
+def wait_for_page_loading(src):
+    soup = BeautifulSoup(src, 'html.parser')
+    display = soup.find('div', attrs={'id':'LoadingTem'})
+    style = display.get('style')
+    return style
+
 driver = webdriver.Firefox()
 driver.get('https://bsrv.bahman.ir/Part.aspx?company=bm')
 
@@ -39,9 +47,11 @@ driver.get('https://bsrv.bahman.ir/Part.aspx?company=bm')
 car_finder = driver.find_element_by_id('drpCarType')
 cars_name = car_finder.text.split('\n')
 
+delay = 60
+
 for car in cars_name:
     """Selecting all car type"""
-    select = wait_for_ajax(driver, (By.ID, 'drpCarType'))
+    select = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'drpCarType')))
     selecting = Select(select)
     selecting.select_by_visible_text(car)
     car_model = driver.find_element_by_id('drpCarModel')
@@ -50,24 +60,38 @@ for car in cars_name:
     if len(carModels) != 0:
         for model in carModels:
             """Selecting all car model"""
-            mselect = wait_for_ajax(driver, (By.ID, 'drpCarModel'))
+            mselect = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'drpCarModel')))
             mselecting = Select(mselect)
             mselecting.select_by_visible_text(model)
 
             for char in persian_character.ppe_characters:
                 """Inserting all character to input tag"""
-                char_input = wait_for_ajax(driver, (By.ID, 'txtPartName'))
+                char_input = driver.find_element_by_id('txtPartName')
                 char_input.send_keys(char)
-                clicker = wait_for_ajax(driver, (By.ID, 'btnSearch'))
-                clicker.click()
+                WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID,'btnSearch'))).click()
+                try:
+                    while wait_for_page_loading(driver.page_source) != 'display: none':
+                        continue
+                except:
+                    time.sleep(0.2)
                 num_page = number_of_page()
-
+                page_src_before = bytearray(driver.page_source, 'utf-8')
+                hash_page_before = hashlib.sha256(page_src_before).hexdigest()
+                print(hash_page_before)
                 if num_page:
                     for page in range(num_page):
                         """Scraping all pages"""
                         print(f'I\'m in page: {page + 1}')
-                        next = wait_for_ajax(driver, (By.NAME, 'dtPager$ctl02$ctl00'))
-                        next.click()
+                        WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.NAME, 'dtPager$ctl02$ctl00'))).click()
+                        page_src_after = bytearray(driver.page_source, 'utf-8')
+                        hash_page_after = hashlib.sha256(page_src_after).hexdigest()
+                        print(hash_page_after)
+                        while True:
+                            if hash_page_before == hash_page_after:
+                                page_src_after = bytearray(driver.page_source, 'utf-8')
+                                hash_page_after = hashlib.sha256(page_src_after).hexdigest()
+                            else:
+                                break
 
                 # clearing the input tag
                 char_input = driver.find_element_by_id('txtPartName')
